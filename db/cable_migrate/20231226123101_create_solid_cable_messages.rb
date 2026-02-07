8b9ebb70edf7b6d43324d8da4926b1000b7b483b
ginanjartg <ginanjar0822@gmail.com>
2026-02-07 11:23:51 +0700

    ya

--- db/cable_migrate/20231226123101_create_solid_cable_messages.rb
+++ db/cable_migrate/20231226123101_create_solid_cable_messages.rb
@@ -2,10 +2,11 @@
 class CreateSolidCableMessages < ActiveRecord::Migration[7.1]
   def change
     create_table :solid_cable_messages do |t|
+      t.integer :channel_hash, null: false, limit: 8
       t.binary :channel, null: false, limit: 1024
       t.binary :payload, null: false, limit: 536870912
       t.datetime :created_at, null: false
-      t.index [ :channel, :created_at ], name: "index_solid_cable_messages_on_channel_and_created_at"
+      t.index [ :channel_hash, :created_at ], name: "index_solid_cable_messages_on_channel_hash_and_created_at"
       t.index [ :created_at ], name: "index_solid_cable_messages_on_created_at"
     end
   end
